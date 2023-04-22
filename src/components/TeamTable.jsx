6ea4d2f77851a4876c5f6523bebf3da55791adb1
kolashah <105250729+kolashah@users.noreply.github.com>
2023-04-21 20:25:54 -0400

    suspended cases work

--- src/components/TeamTable.jsx
+++ src/components/TeamTable.jsx
@@ -2,24 +2,38 @@ import React from 'react';
 import PlayerTable from './PlayerTable';
 import '../styles/App.css';
 
+function findOddsByLine(altLines, optimalLine) {
+  return altLines.find((alt) => alt.line === optimalLine);
+}
+
 function processData(data, groupedAlts) {
   return data.map((row) => {
     const { statType, line } = row;
+
+    //check if optimal line exists
     const altLines = groupedAlts[statType] || [];
-    console.log(altLines);
     const altLineValues = altLines.map((alt) => alt.line);
     const optimalLineExists = altLineValues.includes(line);
 
+    //check if over/under/push odds have one over 40%
+    const optimalLine = row.line;
+    const {
+      overOdds = 0,
+      underOdds = 0,
+      pushOdds = 0,
+    } = findOddsByLine(altLines, optimalLine) ?? {};
+    const oddsAboveForty = Math.max(overOdds, underOdds, pushOdds) <= 0.4;
+    console.log(oddsAboveForty);
     return {
       ...row,
       highLine: Math.max(row.line, ...altLineValues),
       lowLine: Math.min(row.line, ...altLineValues),
-      marketSuspended: !optimalLineExists ? 1 : row.marketSuspended,
+      marketSuspended: !optimalLineExists || oddsAboveForty ? 1 : row.marketSuspended,
     };
   });
 }
 
-function TeamTables({ groupedData, groupedAlts }) {
+export default function TeamTables({ groupedData, groupedAlts }) {
   return (
     <div>
       {Object.entries(groupedData).map(([team, players]) => (
@@ -56,5 +70,3 @@ function TeamTables({ groupedData, groupedAlts }) {
     </div>
   );
 }
-
-export default TeamTables;
